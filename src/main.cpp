ed2bfc146344d30e0760132e93687558105b4e00
Krzysztof Marciniak <kmarciniak@apptension.com>
2015-05-10 16:23:46 +0200

    Restored mouse movement

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,29 @@
 #include <cstdio>
 #include <cstdlib>
+
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
+
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtx/transform.hpp>
+
+#include <assimp/Importer.hpp>      // C++ importer interface
+#include <assimp/scene.h>           // Output data structure
+#include <assimp/postprocess.h>     // Post processing fla
+
+#include "../include/Model.hpp"
 
 #include <string>
 #include <vector>
 #include <fstream>
 using namespace glm;
+
+// ^ to-do: tekstury, uv-ki
+
+#define WINDOW_WIDTH	800
+#define WINDOW_HEIGHT	600
+#define HALF_PI 1.57f
 
 GLuint LoadShaders(const char * vertex_file_path,const char * fragment_file_path){
  
@@ -87,6 +101,28 @@ GLuint LoadShaders(const char * vertex_file_path,const char * fragment_file_path
     return ProgramID;
 }
 
+/*
+		
+to-do:
+=======
+- sprawdzic jak wygladalo main.cpp gdy dzialala kamera
+- tekstury
+- crytek sponza
+- ewentualne poprawki kamery (kwaterniony?)
+
+*/
+
+/*
+	to-do:
+	0) naprawiæ build dla Win32
+	2) kamera
+	3) cegui -> zbudowaæ + wgraæ
+	5) pomys³ na pamiêæ (z placement new)
+	6) papery o GI -> g³ównie jakieœ state-of-the-art, ale te¿ photon mapping (o photon splashingu), deep g-buffer radiosity itd.
+	7) kolor s³oñca (atmospheric attenuation itp.)
+	8) tiled deferred rendering
+*/
+
 int main(int argc, char *argv[]) {
 	if( !glfwInit() ) {
 		fprintf( stderr, "Failed to initialize GLFW.\n" );
@@ -100,7 +136,7 @@ int main(int argc, char *argv[]) {
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
  
 	GLFWwindow* window;
-	window = glfwCreateWindow( 800, 600, "Freya :: Global Illumination Playground", NULL, NULL); 
+	window = glfwCreateWindow( WINDOW_WIDTH, WINDOW_HEIGHT, "Freya :: Global Illumination Playground", NULL, NULL); 
 
 	if( window == NULL ) {
 		fprintf( stderr, "Failed to open GLFW window.\n" );
@@ -123,34 +159,66 @@ int main(int argc, char *argv[]) {
 
 	glfwSetInputMode(window, GLFW_STICKY_KEYS, GL_TRUE);
  
-	static const GLfloat g_vertex_buffer_data[] = {
-	   -1.0f, -1.0f, 0.0f,
-	   1.0f, -1.0f, 0.0f,
-	   0.0f,  1.0f, 0.0f,
-	};
+	Model model;
 
-	GLuint vertexbuffer;
-	glGenBuffers(1, &vertexbuffer);
-	glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(g_vertex_buffer_data), g_vertex_buffer_data, GL_STATIC_DRAW);
+	printf("loading model...\n");
+	model.load("../res/models/stanford-bunny/bunny.obj");
+	printf("model loaded\n");
 
-	GLuint programID = LoadShaders( "../res/shader/triangle_vertex.glsl", "../res/shader/triangle_fragment.glsl" );
+	GLuint programID = LoadShaders( "../res/shaders/triangle_vertex.glsl", "../res/shaders/triangle_fragment.glsl" );
+
+	glm::vec3 position = glm::vec3( 0, 0, 5 );
+	// horizontal angle : toward -Z
+	float horizontalAngle = 3.14f;
+	// vertical angle : 0, look at the horizon
+	float verticalAngle = 0.0f;
+	// Initial Field of View
+	float FoV = 45.0f;
+ 
+	float speed = 0.1f,
+		mouseSpeed = 0.001f,
+		deltaTime;
+	
+	double xpos, ypos, currentTime, lastTime = glfwGetTime();
+	glm::vec3 direction, right, up;
+	glm::mat4	ProjectionMatrix,
+				ViewMatrix,
+				ModelMatrix = glm::mat4(1.0f);  // Changes for each model !
+
+	glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_HIDDEN);
 
 	do{
+		currentTime = glfwGetTime();
+		deltaTime = float(currentTime - lastTime);
+		
+		glfwGetCursorPos(window, &xpos, &ypos);
+		glfwSetCursorPos(window, WINDOW_WIDTH/2, WINDOW_HEIGHT/2);
+
+		horizontalAngle += mouseSpeed * deltaTime * float( WINDOW_WIDTH/2 - xpos );
+		verticalAngle   += mouseSpeed * deltaTime * float( WINDOW_HEIGHT/2 - ypos );
+
+		direction.x = cos(verticalAngle) * sin(horizontalAngle);
+		direction.y = sin(verticalAngle);
+		direction.z = cos(verticalAngle) * cos(horizontalAngle);
+
+		right.x = sin(horizontalAngle - HALF_PI);
+		right.y = 0;
+		right.z = cos(horizontalAngle - HALF_PI);
+
+		up = glm::cross( right, direction );
+
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 		glUseProgram(programID);
 		
-		glm::mat4 Projection = glm::perspective(45.0f, 4.0f / 3.0f, 0.1f, 100.0f);
+		ProjectionMatrix = glm::perspective(FoV, 4.0f / 3.0f, 0.1f, 100.0f);
 		// Camera matrix
-		glm::mat4 View       = glm::lookAt(
-			glm::vec3(4,3,3), // Camera is at (4,3,3), in World Space
-			glm::vec3(0,0,0), // and looks at the origin
-			glm::vec3(0,1,0)  // Head is up (set to 0,-1,0 to look upside-down)
+		ViewMatrix       = glm::lookAt(
+			position,           // Camera is here
+			position+direction, // and looks here : at the same position, plus "direction"
+			up                  // Head is up (set to 0,-1,0 to look upside-down)
 		);
-		// Model matrix : an identity matrix (model will be at the origin)
-		glm::mat4 Model      = glm::mat4(1.0f);  // Changes for each model !
 		// Our ModelViewProjection : multiplication of our 3 matrices
-		glm::mat4 MVP        = Projection * View * Model; // Remember, matrix multiplication is the other way around
+		glm::mat4 MVP        = ProjectionMatrix * ViewMatrix * ModelMatrix; // Remember, matrix multiplication is the other way around
 
 		GLuint MatrixID = glGetUniformLocation(programID, "MVP");
  
@@ -159,23 +227,25 @@ int main(int argc, char *argv[]) {
 		// For each model you render, since the MVP will be different (at least the M part)
 		glUniformMatrix4fv(MatrixID, 1, GL_FALSE, &MVP[0][0]);
 
-		glEnableVertexAttribArray(0);
-		glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
-		glVertexAttribPointer(
-		   0,                  // attribute 0. No particular reason for 0, but must match the layout in the shader.
-		   3,                  // size
-		   GL_FLOAT,           // type
-		   GL_FALSE,           // normalized?
-		   0,                  // stride
-		   (void*)0            // array buffer offset
-		);
- 
-		glDrawArrays(GL_TRIANGLES, 0, 3);
- 
-		glDisableVertexAttribArray(0);
+		model.draw();
 
 		glfwSwapBuffers(window);
 		glfwPollEvents();
+
+		if(glfwGetKey( window, GLFW_KEY_W ) == GLFW_PRESS){
+			position += direction * deltaTime * speed;
+		}
+		if(glfwGetKey( window, GLFW_KEY_S ) == GLFW_PRESS){
+			position -= direction * deltaTime * speed;
+		}
+		// Strafe right
+		if (glfwGetKey( window, GLFW_KEY_D ) == GLFW_PRESS){
+			position += right * deltaTime * speed;
+		}
+		// Strafe left
+		if (glfwGetKey( window, GLFW_KEY_A ) == GLFW_PRESS){
+			position -= right * deltaTime * speed;
+		}
 	} while( glfwGetKey(window, GLFW_KEY_ESCAPE ) != GLFW_PRESS && glfwWindowShouldClose(window) == 0 );
 
 	return 0;
